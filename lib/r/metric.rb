c9e7760db8a8fcce4bd8e0ced524005e9881a994
Keith Rowell <keith@keithrowell.com>
2020-06-18 10:28:09 +1000

    generated gem boilerplate

---
+++ lib/r/metric.rb
@@ -0,0 +1,8 @@
+require "r/metric/version"
+
+module R
+  module Metric
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/r/metric/version.rb
@@ -0,0 +1,5 @@
+module R
+  module Metric
+    VERSION = "0.1.0"
+  end
+end
